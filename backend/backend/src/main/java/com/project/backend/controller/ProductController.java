0806424556b9b0467ea743b23a02fac0d178571e
sangtd <sangtd2005@gmail.com>
2024-08-10 11:20:06 +0700

    Tiếp tục

--- backend/backend/src/main/java/com/project/backend/controller/ProductController.java
+++ backend/backend/src/main/java/com/project/backend/controller/ProductController.java
@@ -1,17 +1,9 @@
 package com.project.backend.controller;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,9 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.project.backend.dtos.ProductDTO;
 
@@ -32,22 +22,8 @@ import jakarta.validation.Valid;
 @RestController
 @RequestMapping("api/v1/products")
 public class ProductController {
-    @GetMapping("")
-    public ResponseEntity<String> getAllProducts(
-            @RequestParam("page") int page,
-            @RequestParam("limit") int limit) {
-        return ResponseEntity.ok(String.format("getAllProducts, page = %d, limit = %d", page, limit));
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<String> getProductById(@PathVariable("id") String productId) {
-        return ResponseEntity.ok("Product with Id: " + productId);
-    }
-
-    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE) // Chuyền file
-    public ResponseEntity<?> createProduct(@Valid @RequestBody ProductDTO productDTO,
-            @RequestPart("file") MultipartFile file,
-            BindingResult result) {
+    @PostMapping("")
+    public ResponseEntity<?> creaetProduct(@Valid @RequestBody ProductDTO productDTO, BindingResult result) {
         try {
             if (result.hasErrors()) {
                 List<String> errorMessages = result.getFieldErrors()
@@ -56,46 +32,26 @@ public class ProductController {
                         .toList();
                 return ResponseEntity.badRequest().body(errorMessages);
             }
-            if (file != null) {
-                // Kiểm tra kích thức file và định dạng
-                if (file.getSize() > 10 * 1024 * 1024) { // Lớn hơn 10Mb
-                    return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
-                            .body("File is too large! Maximum is 10Mb");
-                }
-                String contentType = file.getContentType();
-                if (contentType == null || !contentType.startsWith("image/")) {
-                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                            .body("File must be a image");
-                }
-                // Lưu file và cập nhật thumnail trong DTO
-                String filename = storeFile(file);
-                // Lưu vào đối tượng product trong database
-            }
-            return ResponseEntity.ok("create products");
-        } catch (Exception error) {
-            return ResponseEntity.badRequest().body(error.getMessage());
+            return ResponseEntity.ok("Prodcut created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
-    private String storeFile(MultipartFile file) throws IOException {
-        String filename = StringUtils.cleanPath(file.getOriginalFilename());
-        // Thêm UUID vào trước tên file để đảm bảo tên file là duy nhât
-        String uniqueFilename = UUID.randomUUID().toString() + "_" + filename;
-        // Đường dẫn đến thư mục lưu file
-        Path uploadDir = Paths.get("uploads");
-        // Kiểm tra và tạo thư mục nếu nó không tồn tại
-        if (!Files.exists(uploadDir)) {
-            Files.createDirectories(uploadDir);
-        }
-        // Đường dẫn đầy đủ đến file
-        Path destination = Paths.get(uploadDir.toString(), uniqueFilename);
-        // Sao chép file vào thư mục đích
-        Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
-        return uniqueFilename;
+    @GetMapping("")
+    public ResponseEntity<String> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit) {
+        return ResponseEntity.ok("getProducts here");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getProductById(@PathVariable("id") String productId) {
+        return ResponseEntity.ok("Product with ID: " + productId);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        return ResponseEntity.ok(String.format("Delete product with id is %d", id));
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body("Prodcut deletes successfully");
     }
 }
--- backend/backend/src/main/java/com/project/backend/dtos/ProductDTO.java
+++ backend/backend/src/main/java/com/project/backend/dtos/ProductDTO.java
@@ -4,21 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
-    @NotEmpty(message = "Product's name can not empty")
+    @NotBlank(message = "Title is required")
     @Size(min = 3, max = 200, message = "Title must be between 3 and 200 characters")
     private String name;
 
     @Min(value = 0, message = "Price must be greater than or equal to 0")
     @Max(value = 10000000, message = "Price must be less than or equal to 10,000,000")
-    private float price;
+    private Float price;
 
-    private String thumbnail;
+    private String thumnail;
     private String description;
 
     @JsonProperty("category_id")
