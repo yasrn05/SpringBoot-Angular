27afb9203c1ecec6120f3a7130a7daa247818c01
sangtd <sangtd2005@gmail.com>
2024-08-14 15:25:56 +0700

    [ADD] model user, role

---
+++ backend/backend/src/main/java/com/project/backend/models/User.java
@@ -0,0 +1,49 @@
+package com.project.backend.models;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "full_name", length = 100)
+    private String fullName;
+
+    @Column(name = "phone_number", length = 10, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "address", length = 200)
+    private String address;
+
+    @Column(name = "password", length = 200, nullable = false)
+    private String password;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name = "facebook_account_id")
+    private int facebookAccountId;
+
+    @Column(name = "google_account_id")
+    private int googleAccountId;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+}
