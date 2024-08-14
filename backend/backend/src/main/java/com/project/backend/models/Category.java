e365d54838758936a7e5fbbea99ca63cdda86dad
sangtd <sangtd2005@gmail.com>
2024-08-14 14:22:22 +0700

    [ADD] Model Category

---
+++ backend/backend/src/main/java/com/project/backend/models/Category.java
@@ -0,0 +1,22 @@
+package com.project.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
