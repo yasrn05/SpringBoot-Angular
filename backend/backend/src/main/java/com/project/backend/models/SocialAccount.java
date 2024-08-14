b182af8b5534f7626ca063579bb6454e56dbe9c8
sangtd <sangtd2005@gmail.com>
2024-08-14 15:47:02 +0700

    [ADD] model social account

---
+++ backend/backend/src/main/java/com/project/backend/models/SocialAccount.java
@@ -0,0 +1,30 @@
+package com.project.backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "social_account")
+public class SocialAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "provider", nullable = false, length = 20)
+    private String provider;
+
+    @Column(name = "provider_id", nullable = false, length = 50)
+    private String providerId;
+
+    @Column(name = "name", length = 150)
+    private String name;
+
+    @Column(name = "email", length = 150)
+    private String email;
+}
