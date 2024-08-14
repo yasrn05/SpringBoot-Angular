dec6ba097e8c0a69e99c14d1beea2a590b057ce6
sangtd <sangtd2005@gmail.com>
2024-08-14 14:39:00 +0700

    [ADD] Model BaseEntity

---
+++ backend/backend/src/main/java/com/project/backend/models/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.project.backend.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
