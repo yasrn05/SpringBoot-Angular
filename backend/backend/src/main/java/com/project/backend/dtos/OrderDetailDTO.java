3e096afa649d5be2505df9f0ba8e93316bec52fc
sangtd <sangtd2005@gmail.com>
2024-08-14 10:22:15 +0700

    [ADD] Vaid cho OrderDetailDTO

--- backend/backend/src/main/java/com/project/backend/dtos/OrderDetailDTO.java
+++ backend/backend/src/main/java/com/project/backend/dtos/OrderDetailDTO.java
@@ -2,22 +2,28 @@ package com.project.backend.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class OrderDetailDTO {
     @JsonProperty("order_id")
+    @Min(value = 1, message = "Order's ID must be > 0")
     private Long orderId;
 
     @JsonProperty("product_id")
+    @Min(value = 1, message = "Product's ID must be > 0")
     private Long productId;
 
+    @Min(value = 0, message = "Price must be >= 0")
     private Long price;
 
     @JsonProperty("number_of_products")
+    @Min(value = 1, message = "Number of product must be >= 1")
     private int numberOfProducts;
 
     @JsonProperty("total_money")
+    @Min(value = 0, message = "Total money must be >= 0")
     private int totalMoney;
 
     private String color;
