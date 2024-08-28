d6b535dd1861b249634611c0caa99b2873831f7e
sangtd <sangtd2005@gmail.com>
2024-08-28 09:43:54 +0700

    [Add] OrderResponse

---
+++ backend/backend/src/main/java/com/project/backend/responses/OrderResponse.java
@@ -0,0 +1,58 @@
+package com.project.backend.responses;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResponse extends BaseResponse {
+    private Long id;
+
+    @JsonProperty("user_id")
+    private Long userId;
+
+    @JsonProperty("fullname")
+    private String fullName;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber;
+
+    private String address;
+    private String note;
+
+    @JsonProperty("order_date")
+    private LocalDateTime orderDate;
+
+    private String status;
+
+    @JsonProperty("total_money")
+    private Float totalMoney;
+
+    @JsonProperty("shipping_method")
+    private String shippingMethod;
+
+    @JsonProperty("shipping_address")
+    private String shippingAddress;
+
+    @JsonProperty("shipping_date")
+    private Date shippingDate;
+
+    @JsonProperty("tracking_number")
+    private String trackingNumber;
+
+    @JsonProperty("payment_method")
+    private String paymentMethod;
+
+    private Boolean activate;
+}
