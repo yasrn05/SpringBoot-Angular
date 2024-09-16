b8d90fd00d121e6fba5cc3f11ca13da5a55bd5e0
sangtd <sangtd2005@gmail.com>
2024-09-16 15:38:31 +0700

    [Add] Order Detail Response

---
+++ backend/backend/src/main/java/com/project/backend/responses/OrderDetailResponse.java
@@ -0,0 +1,48 @@
+package com.project.backend.responses;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.project.backend.models.OrderDetail;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderDetailResponse {
+    private Long id;
+
+    @JsonProperty("oder_id")
+    private Long orderId;
+
+    @JsonProperty("product_id")
+    private Long productId;
+
+    @JsonProperty("price")
+    private Float price;
+
+    @JsonProperty("number_of_products")
+    private int numberOfProducts;
+
+    @JsonProperty("total_money")
+    private float totalMoney;
+
+    @JsonProperty("color")
+    private String color;
+
+    public static OrderDetailResponse fromOrderDetail(OrderDetail orderDetail) {
+        return OrderDetailResponse
+                .builder()
+                .id(orderDetail.getId())
+                .orderId(orderDetail.getOrder().getId())
+                .productId(orderDetail.getProduct().getId())
+                .price(orderDetail.getPrice())
+                .numberOfProducts(orderDetail.getNumberOfProducts())
+                .totalMoney(orderDetail.getTotalMoney())
+                .color(orderDetail.getColor())
+                .build();
+    }
+}
