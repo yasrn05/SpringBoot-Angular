868fa7c0d21fc4c27025b09e52d5ea79fd1980d0
sangtd <sangtd2005@gmail.com>
2024-09-17 21:25:53 +0700

    Bộ khung cho WebSecurityConfig

---
+++ backend/backend/src/main/java/com/project/backend/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.project.backend.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> {
+                    requests.requestMatchers("**")
+                            .permitAll();
+                });
+        return httpSecurity.build();
+    };
+}
