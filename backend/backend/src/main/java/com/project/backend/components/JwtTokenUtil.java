1d85afabf7ca3b4a7cad8c27d3615cdbe088377b
sangtd <sangtd2005@gmail.com>
2024-09-21 11:57:41 +0700

    JWT Token Util

---
+++ backend/backend/src/main/java/com/project/backend/components/JwtTokenUtil.java
@@ -0,0 +1,48 @@
+package com.project.backend.components;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.project.backend.models.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenUtil {
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    @Value("$(jwt.secretKey)")
+    private String secretKey;
+
+    public String generateToken(User user) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("phoneNumber", user.getPhoneNumber());
+        try {
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setSubject(user.getPhoneNumber())
+                    .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000L))
+                    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                    .compact();
+        } catch (Exception e) {
+            System.err.println("Cannot create JWT Token, error: " + e.getMessage());
+            return null;
+        }
+    }
+
+    private Key getSignInKey() {
+        byte[] bytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+}
