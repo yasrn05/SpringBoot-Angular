f708719c9e68e504434a3741abef89930bbf7830
sangtd <sangtd2005@gmail.com>
2024-10-08 10:39:55 +0700

    Import các biến còn lại cho Register

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -10,13 +10,33 @@ export class RegisterComponent {
   phone: string;
   password: string;
   retypePassword: string;
+  fullName: string;
+  dateOfBirth: Date;
+  address: string;
+  isAcceped: boolean;
 
   constructor() {
     this.phone = '';
     this.password = '';
     this.retypePassword = '';
+    this.fullName = '';
+    this.dateOfBirth = new Date();
+    this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear() - 18);
+    this.address = '';
+    this.isAcceped = false;
   }
   onPhoneChange() {
     console.log(`Phone typed: ${this.phone}`)
+  }
+  register() {
+    const message =
+      `Phone: ${this.phone} \n` +
+      `Password: ${this.password} \n` +
+      `RetypePassword: ${this.retypePassword} \n` +
+      `FullName: ${this.fullName} \n` +
+      `DateOfBirth: ${this.dateOfBirth} \n` +
+      `Address: ${this.address} \n` +
+      `IsAcceped: ${this.isAcceped}`;
+    alert(message)
   }
 }
