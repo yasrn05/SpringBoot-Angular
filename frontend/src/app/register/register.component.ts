3faaa094a13903133de173d2790137990b54f3fc
sangtd <sangtd2005@gmail.com>
2024-10-08 11:32:17 +0700

    Valid age trong Register

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -39,6 +39,23 @@ export class RegisterComponent {
       this.registerForm.form.controls['retypePassword'].setErrors(null);
     }
   }
+  checkAge() {
+    if (this.dateOfBirth) {
+      const today = new Date();
+      const birthDate = new Date(this.dateOfBirth);
+      let age = today.getFullYear() - birthDate.getFullYear();
+      const monthDiff = today.getMonth() - birthDate.getMonth();
+      if (monthDiff < 0 || (monthDiff == 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        this.registerForm.form.controls['dateOfBirth'].setErrors({ 'invalidAge': true });
+      } else {
+        this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+      }
+    }
+  }
   register() {
     const message =
       `Phone: ${this.phone} \n` +
